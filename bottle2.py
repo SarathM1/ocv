6f758d34e7f8aa8ea646d4043815fc3e14843631
Laptop <sarathm00@gmail.com>
2016-04-10 23:19:43 +0530

    Working, takes 0.02 secs on lap

--- bottle2.py
+++ bottle2.py
@@ -4,6 +4,7 @@ import numpy as np
 
 def find_bottle():
     img = cv2.imread('parachute3.jpg')
+    e1 = cv2.getTickCount()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray, 0, 255,
                                 cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
@@ -25,12 +26,29 @@ def find_bottle():
     y2 = y+h-50
     label = img[y1:y2, x1:x2]
     cv2.imwrite("label.jpg", label)
-    label_hsv = cv2.cvtColor(label, cv2.COLOR_BGR2HSV)
+    """label_hsv = cv2.cvtColor(label, cv2.COLOR_BGR2HSV)
     blue_low = np.array([107, 50, 50], np.uint8)
     blue_high = np.array([112, 255, 255], np.uint8)
     thresh = cv2.inRange(label_hsv, blue_low, blue_high)
+    """
     cv2.imshow("Label", label)
+
+    gray = cv2.cvtColor(label, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+                                   cv2.THRESH_BINARY_INV, 11, 15)
+
+    kernel = np.ones((5, 5), np.uint8)
+    cv2.dilate(thresh, kernel, iterations=1)
     cv2.imshow("thresh", thresh)
+    (_, cnts, _) = cv2.findContours(thresh, cv2.RETR_TREE,
+                                    cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(label, cnts, -1, (0, 0, 255), 1)
+    img[y1:y2, x1:x2] = label
+    e2 = cv2.getTickCount()
+    time = (e2 - e1) / cv2.getTickFrequency()
+    print time
+    cv2.imshow("Final", img)
+    cv2.imwrite("final2.jpg", img)
     cv2.waitKey(0)
 
 if __name__ == '__main__':
